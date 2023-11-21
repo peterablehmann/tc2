a6c6b81858b22c036897f40404684a7d661bd6d3
Peter Lehmann <peter@lehmann.de>
2023-11-21 22:24:05 +0100

    Add ntfy.sh

---
+++ modules/ntfy.nix
@@ -0,0 +1,30 @@
+let
+  domain = "ntfy.xnee.net";
+in
+{
+  security.acme.certs."${domain}" = { };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    virtualHosts."${domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:2586";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+  services.ntfy-sh = {
+    enable = true;
+    settings = {
+      listen-http = "127.0.0.1:2586";
+      base-url = "https://ntfy.xnee.net";
+    };
+  };
+}
